462b2b7b8cccacd539e00fc66d149a631718a752
JessicaReinken <jessica.reinken@tutanota.com>
2016-09-16 15:55:33 +0200

    The first tutorial steps for JS

---
+++ JS-Playground_1.js
@@ -0,0 +1,13 @@
+//First function to play a little around
+function myFunction() {
+    document.getElementById('demo').innerHTML = "Paragraph changed";
+}
+
+//Opens a little window with the result of the operation
+window.alert(24+6);
+
+//writes into the document the result of the operation at the position of the script call in the html document
+document.write(5+6);
+
+//write the result of the operation into the browser console (F12)
+console.log(8+5);
---
+++ JS-Playground_2.js
@@ -0,0 +1,13 @@
+var x = 5;
+var y = 12;
+var z = x + y;
+
+//Testing the variable result
+//window.alert(z);
+
+var price1 = 5;
+var price2 = 12;
+var total = price1 + price2;
+
+//Testing the variable result
+//window.alert(z + " " + total);
