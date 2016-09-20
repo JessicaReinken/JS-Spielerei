9a60c63a2eb42d9b9a41110173832eb125de7892
JessicaReinken <jessica.reinken@tutanota.com>
2016-09-20 14:48:48 +0200

    Change View in the HTML-file. More excercises

--- 11 Assignment.js
+++ 11 Assignment.js
@@ -8,29 +8,33 @@ function excercise_11_01(p_id) {
 }
 
 // Exercise 02
-// Display the difference of 10 - 5, using two variables x and y.
+// Use the -= operator to subtract a value of 5 from the variable x.
 function excercise_11_02(p_id) {
-	var x = 10, y = 5;
-    document.getElementById(p_id).innerHTML = x - y;
+	var x = 10;
+	x -= 5;
+    document.getElementById(p_id).innerHTML = x;
 }
 
 // Exercise 03
-// Display the product of 10 * 5, using two variables x and y.
+// Use the *= operator to multiply the variable x with 5.
 function excercise_11_03(p_id) {
-	var x = 10, y = 5;
-    document.getElementById(p_id).innerHTML = x * y;
+	var x = 10;
+	x *= 5;
+    document.getElementById(p_id).innerHTML = x;
 }
 
 // Exercise 04
-// Display the result of 10 / 5, using two variables x and y.
+// Use the /= operator to divide the variable x with 5.
 function excercise_11_04(p_id) {
-	var x = 10, y = 5;
-    document.getElementById(p_id).innerHTML = x / y;
+	var x = 10;
+	x /= 5;
+    document.getElementById(p_id).innerHTML = x;
 }
 
 // Exercise 05
 // Find the remainder when 15 is divided by 9, using two variables x and y.
 function excercise_11_05(p_id) {
-	var x = 15, y = 9;
-    document.getElementById(p_id).innerHTML = x % y;
+	var x = 10;
+	x %= 3;
+    document.getElementById(p_id).innerHTML = x;
 }
