13104cfce9961244989372b5b44cbbddb9cfdf18
JessicaReinken <jessica.reinken@tutanota.com>
2016-09-20 08:55:54 +0200

    First exercises from w3schools

---
+++ 11 Assignment.js
@@ -0,0 +1,36 @@
+// Arithmetic
+// Exercise 01
+// Use the += operator to add a value of 5 to the variable x.
+function excercise_11_01(p_id) {
+	var x = 10;
+	x += 5;
+    document.getElementById(p_id).innerHTML = x;
+}
+
+// Exercise 02
+// Display the difference of 10 - 5, using two variables x and y.
+function excercise_11_02(p_id) {
+	var x = 10, y = 5;
+    document.getElementById(p_id).innerHTML = x - y;
+}
+
+// Exercise 03
+// Display the product of 10 * 5, using two variables x and y.
+function excercise_11_03(p_id) {
+	var x = 10, y = 5;
+    document.getElementById(p_id).innerHTML = x * y;
+}
+
+// Exercise 04
+// Display the result of 10 / 5, using two variables x and y.
+function excercise_11_04(p_id) {
+	var x = 10, y = 5;
+    document.getElementById(p_id).innerHTML = x / y;
+}
+
+// Exercise 05
+// Find the remainder when 15 is divided by 9, using two variables x and y.
+function excercise_11_05(p_id) {
+	var x = 15, y = 9;
+    document.getElementById(p_id).innerHTML = x % y;
+}
